194d665199b5b7df511bd12b31fa9d1e34e099c3
QuynhXEM <quynh@xem.edu.vn>
2025-07-08 11:57:20 +0700

    Add: Theme Mode

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { ModeToggle } from "@/commons/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
 
 const Home = () => {
@@ -7,6 +9,9 @@ const Home = () => {
   return (
     <div>
       <h1>{t("title")}</h1>
+      <Button>{t("title")}</Button>
+      <ModeToggle/>
+      <div className="w-full h-screen bg-zinc-950 dark:bg-white" />
     </div>
   );
 };
